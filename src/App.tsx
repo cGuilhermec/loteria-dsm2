1480dab2ccad3a6af011a2c5fe19f12bc3ec17b0
cGuilhermec <guilherme-9951@live.com>
2024-04-14 15:14:05 -0300

    aula do arley, tarefa da loteria

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,10 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { buscarDadosLoteria } from './api/loteria';
+import { Provider } from "./contexts";
+import Principal from "./pages/Principal";
 
-function App() {
- 
-  const [dados, setDados] = useState(null);
-
-  useEffect(() => {
-
-    async function carregarDados() {
-      
-      try {
-
-        const dadosLoteria = await buscarDadosLoteria();
-
-        setDados(dadosLoteria);
-
-      } catch (error) {
-        
-      }
-
-    }
-
-    carregarDados();
-  }, [])
-
+export default function App() {
   return (
-    <div>
-      {dados && (
-        <div>
-          <h2>Resultados da Loteria</h2>
-          {Object.keys(dados).map((jogo, index) => (
-            <div key={index}>
-              <h3>{jogo}</h3>
-              {/* <p>Data do sorteio: {dados}.</p> */}
-              {/* <p>Número do concurso: {dados[jogo]}</p> */}
-              {/* Adicione mais propriedades conforme necessário */}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-    
-
-  )
-
+    <Provider>
+      <Principal />
+    </Provider>
+  );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import "@fontsource/roboto";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+root.render(<App />);
 
